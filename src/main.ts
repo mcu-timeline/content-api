a0210f80112d2c98b5f14bd3d757bab6fc17a31e
Nikodem Wrona <wronanikodem@protonmail.com>
2022-11-21 17:20:52 +0100

    chore(mvp): use PORT env in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Config } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3001);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+
+  const configService = app.get(ConfigService<Config>);
+  const port = configService.get('PORT');
+
+  await app.listen(port, async () => {
+    console.log(`Server started on ${await app.getUrl()}`);
+  });
 }
 bootstrap();
