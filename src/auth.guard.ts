99020bf4fa3fa39cc4562f784bf5244f2237a675
nikodem <wronanikodem@protonmail.com>
2022-12-02 20:44:47 +0100

    feat(mvp): fix typescript errors, fix decodedTokenPromise function

--- src/auth.guard.ts
+++ src/auth.guard.ts
@@ -34,21 +34,20 @@ export class JwtAuthGuard {
     });
 
     const getKey = (header, callback): void => {
-      client.getSigningKey(header.kid, function(error, key) {
-        // @ts-ignore
-        const signingKey = key.publicKey || key.rsaPublicKey;
+      client.getSigningKey(header.kid, (error: Error, key: jwksClient.RsaSigningKey) => {
+        const signingKey = key.getPublicKey() || key.rsaPublicKey;
         callback(null, signingKey);
       });
     }
 
-    const decodedTokenPromise = new Promise((resolve) => {
+    const decodedTokenPromise = new Promise((resolve, reject) => {
       verify(token, getKey, {
         audience: this.configService.get('AUTH0_AUDIENCE'),
         issuer: this.configService.get('AUTH0_ISSUER_URL'),
         algorithms: ["RS256"]
       }, (error, decoded) => {
         if (error) {
-          throw new Error(error.message)
+          reject(error)
         }
         if (decoded) {
           resolve(decoded);
@@ -58,10 +57,6 @@ export class JwtAuthGuard {
 
     const decodedToken = await decodedTokenPromise;
 
-    if (decodedToken) {
-      return true;
-    } else {
-      return false;
-    }
+    return !!decodedToken;
   }
 }
